59d0eee5b53381c279b5423a3bd0647b00057908
William Li <williamli@19degrees.io>
2015-06-01 16:40:27 +0800

    [ v0.2.2] expose persistent context via namedContext

--- simple-schema-enforcer.js
+++ simple-schema-enforcer.js
@@ -1,23 +1,35 @@
 // exported
-SimpleSchemaEnforcer = function(simpleSchema, errorHandlers) {
+SimpleSchemaEnforcer = function (simpleSchema, errorHandlers) {
   var self = this;
 
   self._simpleSchema = simpleSchema;
   self._errorHandlers = errorHandlers;
 
+  self._context = self._simpleSchema.namedContext('simpleSchemaEnforcerContext');
+
 
 }
 
-SimpleSchemaEnforcer.prototype.validate = function(data, keys) {
+SimpleSchemaEnforcer.prototype.getContext = function () {
+  var self = this;
+  
+  return self._context;
+}
+
+SimpleSchemaEnforcer.prototype.validate = function (data, keys) {
   var self = this;
 
-  var context = self._simpleSchema.newContext();
+  var context = self._context;
 
-  _.each(self._errorHandlers, function (e) 
-    {
-      if (e.inputID !== undefined)
-        $('#' + e.inputID + '.simple-schema-enforcer-error').removeClass('simple-schema-enforcer-error');
-    });
+  context.resetValidation();
+
+  if (Meteor.isClient) {
+    _.each(self._errorHandlers, function (e) 
+      {
+        if (e.inputID !== undefined)
+          $('#' + e.inputID + '.simple-schema-enforcer-error').removeClass('simple-schema-enforcer-error');
+      });
+  }
 
   if (keys !== undefined) {
     _.each(keys, function(k) {
@@ -36,34 +48,36 @@ SimpleSchemaEnforcer.prototype.validate = function(data, keys) {
       },''), context.invalidKeys());
 }
 
-SimpleSchemaEnforcer.prototype.parseError = function(meteorError) {
-  var self = this;
+if (Meteor.isClient) {
+  SimpleSchemaEnforcer.prototype.parseError = function (meteorError) {
+    var self = this;
 
-  console.error('Error:', meteorError.error, '-', meteorError.reason);
+    console.error('Error:', meteorError.error, '-', meteorError.reason);
 
-  var errorDetails = meteorError.details;
+    var errorDetails = meteorError.details;
 
-  
-
-
-  _.each(errorDetails, function(err){
     
-    err.name = err.name.replace(/(\.(\d)+)/g,'.$');
 
-    console.error('Schema Error:', err.type,'@', err.name);
-    
-    var inputID = self._errorHandlers[err.name]['inputID'];
-    
-    var handler = self._errorHandlers[err.name]['handlers'][err.type];
 
-    console.log('Error Message:', handler.message);
+    _.each(errorDetails, function(err){
+      
+      err.name = err.name.replace(/(\.(\d)+)/g,'.$');
 
-    if (handler.callback !== undefined)
-      handler.callback(handler.message);
+      console.error('Schema Error:', err.type,'@', err.name);
+      
+      var inputID = self._errorHandlers[err.name]['inputID'];
+      
+      var handler = self._errorHandlers[err.name]['handlers'][err.type];
 
-    if (inputID !== undefined)
-      if (handler.inputHightlight === true)
-        $('#' + inputID).addClass('simple-schema-enforcer-error');
+      console.log('Error Message:', handler.message);
 
-  });
+      if (handler.callback !== undefined)
+        handler.callback(handler.message);
+
+      if (inputID !== undefined)
+        if (handler.inputHightlight === true)
+          $('#' + inputID).addClass('simple-schema-enforcer-error');
+
+    });
+  }
 }
